42337d7b909c4084d87647cdd8c42aae7881fe0a
Hrishikesh Sathe <xxx2050@gmail.com>
2014-06-08 19:54:11 +0530

    Check if internet is connected
    on startup of PC check if internet is connected. if not shutdown the pc

--- check_time.py
+++ check_time.py
@@ -1,7 +1,18 @@
 import os
 import time
+import requests
 
-#retrieve the current system time and check if its in between 4 AM and 5 AM (scheduled time for download)
-hours = time.localtime().tm_hour
-if 4 <= hours <= 5:
-    os.startfile("C:\py_scheduler.py") #start the scheduler
+try:
+    time.sleep(30)
+    
+    #try to get a google page. if it timeouts then internet is not connected and exception is raised
+    response = requests.get('http://74.125.228.100',timeout=5)
+    
+    #retrieve the current system time and check if its in between 4 AM and 5 AM (scheduled time for download)
+    hours = time.localtime().tm_hour
+    if hours == 19:
+        os.startfile("D:\Aptana\workspace\Tutorials\py_scheduler.py") #start the scheduler
+    
+except Exception as e:
+    #Shutdown the system if there is a problem connecting to the internet
+    os.system('shutdown /s')
